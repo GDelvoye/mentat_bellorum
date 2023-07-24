2aae7a7932fb2967becacc4359be0ac16a65a2bc
gauthier.delvoye <gauthier.delvoye@kaizen-solutions.net>
2023-07-24 22:29:19 +0200

    test caracteristique parametriques

--- back/tests/figurine/test_caracteristique.py
+++ back/tests/figurine/test_caracteristique.py
@@ -1,13 +1,33 @@
+import pytest
+
 from back.src.figurine.caracteristique import Caracteristique
 
+testdata = [
+    (
+        Caracteristique(1, 0, 0, 0, 0, 0, 0, 0, 0, 6, 4),
+        Caracteristique(3, 0, 0, 1, 0, 0, 0, 0, 0, 6, 3),
+        Caracteristique(4, 0, 0, 1, 0, 0, 0, 0, 0, 5, 3),
+    ),
+    (
+        Caracteristique(1, 0, 0, -1, 0, 0, 0, 0, 0, 6, 4),
+        Caracteristique(-3, 0, 0, 1, 0, 0, 0, 0, 13, 1, 1),
+        Caracteristique(0, 0, 0, 0, 0, 0, 0, 0, 12, 1, 1),
+    ),
+    (
+        Caracteristique(1, 0, 0, 10, 0, 0, 0, 0, 0, 6, 6),
+        Caracteristique(3, 0, 0, 1, 0, 0, 0, 0, 0, -2, 7),
+        Caracteristique(4, 0, 0, 10, 0, 0, 0, 0, 0, 7, 6),
+    ),
+]
 
-def test_caracteristique():
-    # Given
-    caracteristique1 = Caracteristique(1, 0, 0, 0, 0, 0, 0, 0, 0, 6, 4)
-    caracteristique2 = Caracteristique(3, 0, 0, 1, 0, 0, 0, 0, 0, 6, 3)
-    # When
-    caracteristique3 = caracteristique1 + caracteristique2
-    # Then
-    assert caracteristique3.mouvement == 4
-    assert caracteristique3.sauvegarde == 5
-    assert caracteristique3.sauvegarde_invulnerable == 3
+
+@pytest.mark.parametrize("c1, c2, c_expected", testdata)
+def test_caracteristique(
+    c1: Caracteristique, c2: Caracteristique, c_expected: Caracteristique
+):
+    result = c1 + c2
+    assert result.mouvement == c_expected.mouvement
+    assert result.force == c_expected.force
+    assert result.commandement == c_expected.commandement
+    assert result.sauvegarde == c_expected.sauvegarde
+    assert result.sauvegarde_invulnerable == c_expected.sauvegarde_invulnerable
