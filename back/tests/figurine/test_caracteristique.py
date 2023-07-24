2348f391118783f990207b0efe52a4d0628491f8
gauthier.delvoye <gauthier.delvoye@kaizen-solutions.net>
2023-07-24 03:35:21 +0200

    pytest path issue

--- back/tests/figurine/test_caracteristique.py
+++ back/tests/figurine/test_caracteristique.py
@@ -1,9 +1,3 @@
-import pathlib
-import sys
-
-here = pathlib.Path(__file__).parent.resolve()
-sys.path.append(f"{here}/../../..")
-
 from back.src.figurine.caracteristique import Caracteristique
 
 
