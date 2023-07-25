4ee72a685e9c3698a9643e8f3cccc81b011727dc
gauthier.delvoye <gauthier.delvoye@kaizen-solutions.net>
2023-07-26 00:09:20 +0200

    probabilite individuelles toucher, blesser, svg, infliger 1 blessure

--- back/src/combat/combat_individuel.py
+++ back/src/combat/combat_individuel.py
@@ -1,5 +1,8 @@
 from typing import Optional
 
+from back.src.combat.attaque import AttaqueCac
+from back.src.figurine.caracteristique import Caracteristique
+
 
 def jet_pour_toucher_cac(capa_combat_attaquant: int, capa_combat_defenseur: int) -> int:
     """Tableau des jets pour toucher au corps à corps."""
@@ -35,3 +38,68 @@ def jet_de_sauvegarde(
         return sauvegarde_defenseur
     else:
         return min(7, sauvegarde_defenseur + force_contre_sauvegarde - 3)
+
+
+def probabilite_de_toucher_cac(
+    capa_combat_attaquant: int, capa_combat_defenseur: int
+) -> float:
+    return (7 - jet_pour_toucher_cac(capa_combat_attaquant, capa_combat_defenseur)) / 6
+
+
+def probabilite_de_blesser(force_attaquant: int, endurance_defenseur: int) -> float:
+    valeur_pour_blesser = jet_pour_blesser(force_attaquant, endurance_defenseur)
+    if valeur_pour_blesser is None:
+        return 0
+    assert valeur_pour_blesser is not None
+    return (7 - valeur_pour_blesser) / 6
+
+
+def probabilite_de_rater_sa_sauvegarde(
+    force_attaquant: int,
+    sauvegarde_defenseur: int,
+    malus_sauvegarde: int,
+) -> float:
+    """Si la sauvegarde est de 7, on la rate toujours."""
+    valeur_effective_sauvegarde = jet_de_sauvegarde(
+        force_attaquant,
+        sauvegarde_defenseur,
+        malus_sauvegarde,
+    )
+    return (valeur_effective_sauvegarde - 1) / 6
+
+
+def probabilite_de_rater_sa_sauvegarde_invunerable(
+    sauvegarde_invulnerable_defenseur: int,
+) -> float:
+    """Si la sauvegarde est de 7, on la rate toujours."""
+    valeur_effective_sauvegarde = jet_de_sauvegarde(
+        force_attaquant=0,
+        sauvegarde_defenseur=sauvegarde_invulnerable_defenseur,
+        malus_sauvegarde=0,
+    )
+    return (valeur_effective_sauvegarde - 1) / 6
+
+
+def probabilite_une_attaque_fait_perdre_un_point_de_vie(
+    attaque: AttaqueCac,
+    caracteristique_avec_equipement_defenseur: Caracteristique,
+):
+    """Probabilité d'infliger la perte d'un point de vie lors d'une attaque cac."""
+    probabilite = 1.0
+    probabilite *= probabilite_de_toucher_cac(
+        attaque.caracteristique.capa_combat,
+        caracteristique_avec_equipement_defenseur.capa_combat,
+    )
+    probabilite *= probabilite_de_blesser(
+        attaque.caracteristique.force,
+        caracteristique_avec_equipement_defenseur.endurance,
+    )
+    probabilite *= probabilite_de_rater_sa_sauvegarde(
+        attaque.caracteristique.force,
+        caracteristique_avec_equipement_defenseur.sauvegarde,
+        0,
+    )
+    probabilite *= probabilite_de_rater_sa_sauvegarde_invunerable(
+        caracteristique_avec_equipement_defenseur.sauvegarde_invulnerable,
+    )
+    return probabilite
--- back/tests/combat/test_combat_individuel.py
+++ back/tests/combat/test_combat_individuel.py
@@ -1,16 +1,25 @@
 import pytest
+from numpy import isclose
 
+from back.src.combat.attaque import AttaqueCac
 from back.src.combat.combat_individuel import (
     jet_de_sauvegarde,
     jet_pour_blesser,
     jet_pour_toucher_cac,
+    probabilite_de_blesser,
+    probabilite_de_rater_sa_sauvegarde,
+    probabilite_de_rater_sa_sauvegarde_invunerable,
+    probabilite_de_toucher_cac,
+    probabilite_une_attaque_fait_perdre_un_point_de_vie,
 )
+from back.src.figurine.caracteristique import Caracteristique
 
 testdata_jet_pour_toucher_cac = [
     (3, 3, 4),
     (5, 2, 3),
     (4, 7, 4),
     (4, 8, 5),
+    (4, 5, 4),
 ]
 
 
@@ -61,3 +70,80 @@ testdata_jet_de_sauvegarde = [
 def test_jet_de_sauvegarde(f_att: int, svg_def: int, malus_svg: int, expected: int):
     result = jet_de_sauvegarde(f_att, svg_def, malus_svg)
     assert result == expected
+
+
+testdata_proba_toucher_cac = [
+    (3, 3, 1 / 2),
+    (4, 3, 2 / 3),
+    (4, 5, 1 / 2),
+    (4, 8, 1 / 3),
+]
+
+
+@pytest.mark.parametrize("cc_att, cc_def, expected", testdata_proba_toucher_cac)
+def test_probabilite_de_toucher_cac(
+    cc_att: int,
+    cc_def: int,
+    expected: float,
+):
+    result = probabilite_de_toucher_cac(cc_att, cc_def)
+    assert isclose(result, expected)
+
+
+testdata_proba_blesser = [
+    (1, 5, 0),
+    (2, 5, 1 / 6),
+    (3, 5, 1 / 6),
+    (4, 5, 1 / 3),
+    (5, 5, 1 / 2),
+    (6, 5, 2 / 3),
+    (7, 5, 5 / 6),
+]
+
+
+@pytest.mark.parametrize("f_att, e_def, expected", testdata_proba_blesser)
+def test_probabilite_de_blesser(
+    f_att: int,
+    e_def: int,
+    expected: float,
+):
+    result = probabilite_de_blesser(f_att, e_def)
+    assert isclose(result, expected)
+
+
+testdata_proba_sauvegarde = [
+    (3, 5, 0, 2 / 3),
+    (4, 5, 0, 5 / 6),
+    (3, 5, 1, 5 / 6),
+    (4, 5, 1, 1),
+    (10, 1, 10, 1),
+]
+
+
+@pytest.mark.parametrize(
+    "f_att, svg_def, malus_svg, expected", testdata_proba_sauvegarde
+)
+def test_probabilite_de_rater_sa_sauvegarde(
+    f_att: int,
+    svg_def: int,
+    malus_svg: int,
+    expected: float,
+):
+    result = probabilite_de_rater_sa_sauvegarde(f_att, svg_def, malus_svg)
+    assert isclose(result, expected)
+
+
+def test_probabilite_une_attaque_fait_perdre_un_point_de_vie():
+    # Given
+    attaque = AttaqueCac(
+        Caracteristique(4, 3, 3, 3, 3, 1, 3, 1, 7, 7, 7),
+        [],
+    )
+    carac_defenseur = Caracteristique(4, 4, 4, 4, 4, 2, 4, 2, 8, 4, 5)
+    # When
+    result = probabilite_une_attaque_fait_perdre_un_point_de_vie(
+        attaque,
+        carac_defenseur,
+    )
+    # Then
+    assert isclose(result, 1 / 18)
